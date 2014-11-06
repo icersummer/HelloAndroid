8506371f9b6d396f74285b9586bd5993ecc16ec7
icersummer <guofangsky@gmail.com>
2014-11-06 17:08:48 +0800

    init my android project

---
+++ src/com/vjia/helloandroid/HelloAndroidActivity.java
@@ -0,0 +1,22 @@
+package com.vjia.helloandroid;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HelloAndroidActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.hello_android_layout);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.hello_android, menu);
+		return true;
+	}
+
+}
