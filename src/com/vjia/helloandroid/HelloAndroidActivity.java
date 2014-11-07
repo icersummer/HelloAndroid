5dc570c18a4deba8677b32711fdbd1361d448d87
icersummer <guofangsky@gmail.com>
2014-11-07 18:05:28 +0800

    few change

--- src/com/vjia/helloandroid/HelloAndroidActivity.java
+++ src/com/vjia/helloandroid/HelloAndroidActivity.java
@@ -2,6 +2,7 @@ package com.vjia.helloandroid;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class HelloAndroidActivity extends Activity {
@@ -10,6 +11,7 @@ public class HelloAndroidActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.hello_android_layout);
+		Log.v("HelloWorldActivity", "onCreate execute");
 	}
 
 	@Override
